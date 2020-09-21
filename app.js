70e5fad9f66089dc4a4124aab3160841145bbfc0
sendo07 <loakesh.i@gmail.com>
2020-09-21 06:22:42 +0530

    Refactor: Comments to app.js file

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
+/*******************************************************************************************
+ *
+ * Execution       : default node cmd> nodemon app.js
+ * Purpose         : Greetings App
+ *
+ * @description    : Implemention CRUD operation for the Greetings App
+ *
+ * @file           : app.js
+ * @overview       : Greetings App
+ * @module         : Greetings App
+ * @version        : 1.0
+ * @since          : 20/11/2020
+ *
+ * *****************************************************************************************/
+
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
@@ -6,6 +21,7 @@ const usersRoute = require("./routes/users");
 const servicesRoute = require("./routes/services");
 require("dotenv/config");
 require("./config/db");
+require("./config/middlewares");
 
 // Middleware
 app.use(bodyParser.json());
