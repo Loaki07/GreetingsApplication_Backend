c55e0118b9d2e241d9811ad3122ceb6ac02fd963
sendo07 <loakesh.i@gmail.com>
2020-09-26 05:30:16 +0530

    fix: Error while receiving api calls for different HTTP Methods

--- app.js
+++ app.js
@@ -13,31 +13,27 @@
  *
  * *****************************************************************************************/
 
-const express = require("express");
+const express = require('express');
 const app = express();
-const bodyParser = require("body-parser");
-const homepageRoute = require("./routes/homepage");
-const usersRoute = require("./routes/users");
-const servicesRoute = require("./routes/services");
-require("dotenv/config");
-require("./config/db");
+const bodyParser = require('body-parser');
+const homepageRoute = require('./routes/homepage');
+const usersRoute = require('./routes/users');
+const servicesRoute = require('./routes/services');
+require('dotenv/config');
+require('./config/db');
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "*");
-
-  if (req.method === "OPTIONS") {
-    res.header("Access-control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-    return res.status(200);
-  }
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', '*');
+  res.header('Access-Control-Allow-Methods', '*');
   next();
 });
 // Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use("/", homepageRoute);
-app.use("/users", usersRoute);
-app.use("/services", servicesRoute);
+app.use('/', homepageRoute);
+app.use('/users', usersRoute);
+app.use('/services', servicesRoute);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
